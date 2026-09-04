cd0b03d582f0995f28c2d4ed52423aed7119bcc2
lthorpe18 <76230477+lthorpe18@users.noreply.github.com>
2026-09-04 09:26:41 +0100

    Centralise Tournament Day development cache revision

--- v2-preview/apps/_shared/routes.js
+++ v2-preview/apps/_shared/routes.js
@@ -7,5 +7,19 @@ function tournamentDay(participationId){
   params.set('rev',TOURNAMENT_DAY_REV);
   return `./tournament-day.html?${params.toString()}`;
 }
-global.PTCGRoutes={...(global.PTCGRoutes||{}),TOURNAMENT_DAY_REV,tournamentDay};
+function reviseTournamentDayHref(href){
+  try{
+    const url=new URL(href,location.href);
+    if(!/\/tournament-day\.html$/.test(url.pathname))return href;
+    url.searchParams.set('rev',TOURNAMENT_DAY_REV);
+    return `${url.pathname.split('/').pop()}?${url.searchParams.toString()}`;
+  }catch{return href}
+}
+document.addEventListener('click',event=>{
+  const link=event.target?.closest?.('a[href]');
+  if(!link)return;
+  const revised=reviseTournamentDayHref(link.getAttribute('href'));
+  if(revised!==link.getAttribute('href'))link.setAttribute('href',revised);
+},true);
+global.PTCGRoutes={...(global.PTCGRoutes||{}),TOURNAMENT_DAY_REV,tournamentDay,reviseTournamentDayHref};
 })(window);
